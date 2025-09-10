925b71383808f9decbec60a574bc78cee3d809c1
a <justinc1127@nycstudents.net>
2025-09-10 10:20:11 -0400

    Update test.py

--- test.py
+++ test.py
@@ -2,11 +2,26 @@ import turtle
 from turtle import *
 t = Turtle()
 
+# turtle.done()
+#define the functin name then pass arguements/parameter
+# def add(x,y):
+#     print(x+y)
+# def square(x):
+#     t.forward(125)
+#     t.left(90)
+#     t.forward(100)
+#     t.left(90)
 
-t.shape('turtle')
+# square(50)
+# turtle.done()
+# for i in range(1,4):
+#     print(i)
 
-t.forward(200)
-t.left(120)
-t.forward(200)
-t.right(120)
+def rectangle():
+    t.forward(125)
+    t.left(90)
+    t.forward(100)
+    t.left(90)
+    t.width(125)
+    
 turtle.done()
