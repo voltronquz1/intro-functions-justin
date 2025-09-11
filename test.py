eb39a8351dfe361e91cc7300ed068e8d9d3f63fa
a <justinc1127@nycstudents.net>
2025-09-11 10:25:23 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import turtle
 from turtle import *
 t = Turtle()
+t.speed(100)
 
 # turtle.done()
 #define the functin name then pass arguements/parameter
@@ -21,6 +22,37 @@ t = Turtle()
 #     t.forward(90)
 #     t.right(120)
 
+""" for i in range (0,60):
+    t.forward(100)
+    t.left(90)
+    t.right(5)
+    t.speed(100)
+    t.forward(200)
+    t.left(120) """
+
+# for i in range(60):
+#     def square(x):
+#         t.forward(x)
+#         t.left(90)
+#         t.forward(x)
+#         t.left(90)
+#         t.forward(x)
+#         t.left(90)
+#         t.forward(x)
+#         t.left(90)
+#         t.right(5)
+#     square(200)
+
+
+"""         t.forward(x)
+        t.left(90)
+        t.forward(x)
+        t.left(90)
+        t.forward(x)
+        t.left(90)
+        t.forward(x)
+        t.left(90)
+        square(200) """
 
 
 turtle.done()
